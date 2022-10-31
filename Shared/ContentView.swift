d48df8328c2094fca7cc64320408f2172e6bf41c
twinfo <twinfo@126.com>
2022-10-31 10:16:56 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 何广军 on 2022/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, twinfo!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by 何广军 on 2022/7/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
